Fix normalization values

medical_diffusion/data/datasets/dataset_simple_2d.py
@@ -49,7 +49,7 @@ class SimpleDataset2D(data.Dataset):
                     # Normalize(), # [0, 1.0]
                     # T.ConvertImageDtype(torch.float),
                     T.Normalize(
-                        mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+                        mean=0.5, std=0.5
                     ),  # WARNING: mean and std are not the target values but rather the values to subtract and divide by: [0, 1] -> [0-0.5, 1-0.5]/0.5 -> [-1, 1]
                 ]
             )
@@ -284,6 +284,8 @@ class BTXRD_Dataset(SimpleDataset2D):
             json_path = os.path.join(self.json_dir, json_name)
 
             if not os.path.exists(json_path):
+                print(json_path)
+                print(f"ERROR: No json for {fname}")
                 continue
 
             with open(json_path, "r") as f:

scripts/evaluate_latent_embedder.py
@@ -54,7 +54,7 @@ pil2torch = (
 ds_real = BTXRD_Dataset(
     json_dir="./medical_diffusion/data/datasets/BTXRD/Annotations",
     split_path="./medical_diffusion/data/datasets/data-split.json",
-    path_root="./medical_diffusion/data/datasets/BTXRD/final_patched_BTXRD",
+    path_root="./medical_diffusion/data/datasets/final_patched_BTXRD",
     split_mode="test",
     image_resize=(224, 224),
 )
@@ -112,7 +112,7 @@ for real_batch in tqdm(dm_real):
     imgs_real_batch = real_batch["source"].to(device)
 
     imgs_real_batch = tF.normalize(
-        imgs_real_batch / 255, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+        imgs_real_batch / 255, mean=0.5, std=0.5
     )  # [0, 255] -> [-1, 1]
     with torch.no_grad():
         imgs_fake_batch = model(imgs_real_batch)[0].clamp(-1, 1)